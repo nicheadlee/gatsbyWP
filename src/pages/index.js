f0eb235af7ac4832c30fa00f7ba541e5c438764c
Nic Headlee <nicheadlee@gmail.com>
2020-01-02 15:54:57 -0700

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,82 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link>
+//   </Layout>
+// )
 
-export default IndexPage
+// export default IndexPage
+
+class Homepage extends Component {
+  render() {
+    const data = this.props.data
+
+    return (
+      <>
+        <div>
+          <h1>Pages</h1>
+          {data.allWordpressPage.edges.map(({ node }) => (
+            <div key={node.slug}>
+              <Link to={node.slug}>
+                <h2>{node.title}</h2>
+              </Link>
+              <h3>{node.excerpt}</h3>
+            </div>
+          ))}
+        </div>
+
+        <h1>Posts</h1>
+        {data.allWordpressPost.edges.map(({ node }) => (
+          <div key={node.slug}>
+            <Link to={node.slug}>
+              <h2>{node.title}</h2>
+            </Link>
+            <h3>{node.excerpt}</h3>
+          </div>
+        ))}
+      </>
+    )
+  }
+}
+
+export default Homepage
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+    allWordpressPost {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
